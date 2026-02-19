e0810645640285bead7b9bfa731221ff3bed40a8
mohipal92 <mohipalmaahi4050@gmail.com>
2026-02-19 15:52:53 +0530

    url problem

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blog-by-mk.onrender.com",
+  baseURL: "https://blog-by-mk.onrender.com/api",
   withCredentials: true, // IMPORTANT for cookies
 });
 
