ae9d4726e84d33f4806d8d92023f48086593d2b4
mohipal92 <mohipalmaahi4050@gmail.com>
2026-02-19 15:10:34 +0530

    error fixed in api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blog-by-mk.onrender.com/api",
+  baseURL: "https://blog-by-mk.onrender.com",
   withCredentials: true, // IMPORTANT for cookies
 });
 
